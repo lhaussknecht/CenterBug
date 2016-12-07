afde315a27aac9363757a2f1a21118cf8e747190
lhaussknecht <lh@haussknecht.com>
2016-12-07 20:03:37 +0100

    inital commit

---
+++ CenterBug/CenterBug/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CenterBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new DummyViewModel();
+        }
+    }
+
+    public class DummyViewModel
+    {
+        public IList<DummyItems> ListViewItems { get; set; }
+
+        public DummyViewModel()
+        {
+            ListViewItems = new List<DummyItems>();
+            for (int i = 0; i < 50; i++)
+            {
+                ListViewItems.Add(new DummyItems() { PropA = $"A {i}" });
+            }
+
+        }
+    }
+
+    public class DummyItems
+    {
+        public string PropA { get; set; }
+    }
+}
